135bc40738a82c254b21d86ec9e8c41c9ac382c0
dashaua4 <dashaua4@users.noreply.github.com>
2018-05-09 19:54:21 +0300

    Update SQL.php

--- SQL.php
+++ SQL.php
@@ -10,7 +10,7 @@ public static function Instance()
 
 private function _construct()
 { setlocale(LC_ALL,'ru_RU.UTF8');
- $this->db=new PDO('mysql:host=localhost;dbname=mysqldatabase44500','diplomadmin','Alexandra11');
+ $this->db=new PDO('mysql:host=diplomdb-mysqldbserver.mysql.database.azure.com;port=3306;dbname=mysqldatabase44500','diplomadmin','Alexandra11');
  $this->db->exec('SET NAMES UTF8');
  $this->db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
  
@@ -23,7 +23,7 @@ public function Select($query)
 return $q->fetchAll();
 }
 public function Insert($table,$object)
-{ $this->db=new PDO('mysql:host=localhost;dbname=mysqldatabase44500','diplomadmin','Alexandra11');
+{ $this->db=new PDO('mysql:host=diplomdb-mysqldbserver.mysql.database.azure.com;port=3306;dbname=mysqldatabase44500','diplomadmin','Alexandra11');
  
 $columns =array();
 foreach($object as $key=>$value)
@@ -49,7 +49,7 @@ $result= $this->db->lastInsertId();
  
 } 
 public function createTable($table,$object)
-{ $this->db=new PDO('mysql:host=localhost;dbname=mysqldatabase44500','diplomadmin','Alexandra11');
+{ $this->db=new PDO('mysql:host=diplomdb-mysqldbserver.mysql.database.azure.com;port=3306;dbname=mysqldatabase44500','diplomadmin','Alexandra11');
  
   $sqlList = ['CREATE TABLE IF NOT EXISTS Table (
                         id  PRIMARY KEY,
