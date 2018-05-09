44a53ce995badb4ddedfbceaa3054d795dd5d630
dashaua4 <dashaua4@users.noreply.github.com>
2018-05-09 18:35:24 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
 <?php
+$serverName = "diplomdbserver.database.windows.net";
+$connectionOptions = array(
+    "Database" => "Diplom_db",
+    "Uid" => "diplomadmin",
+    "PWD" => "Alexandra11"
+);
+//Establishes the connection
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+if( $conn ) {
+     echo "Connection established.<br />";
+}else{
+     echo "Connection could not be established.<br />";
+     die( print_r( sqlsrv_errors(), true));
+}
 ?>
