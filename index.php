021a18441df627a027368c85e938aba66a64cfbf
dashaua4 <dashaua4@users.noreply.github.com>
2018-05-09 18:50:14 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,24 @@
 <?php
-$serverName = "diplomdbserver.database.windows.net";
-$connectionOptions = array(
-    "Database" => "Diplom_db",
-    "Uid" => "diplomadmin",
-    "PWD" => "Alexandra11"
-);
-//Establishes the connection
-$conn = sqlsrv_connect($serverName, $connectionOptions);
-if( $conn ) {
-     echo "Connection established.<br />";
-}else{
-     echo "Connection could not be established.<br />";
-     die( print_r( sqlsrv_errors(), true));
-}
-?>
+
+include_once('curl_query.php');
+include_once('simple_html_dom.php');
+include_once('Mysql.php');
+$html=curl_get('https://meblihit.com.ua/catalog/modul%60na_systema_ofys/');
+
+$sql=SQL::Instance();
+$dom=str_get_html($html);
+$tables=$dom->find('.name_product');
+foreach($tables as $table)
+{ 
+
+$tobd=array();
+$a=$table->find('a',0);
+
+	$tobd['name']=$a->plaintext;
+	$one=curl_get('https://meblihit.com.ua'.$a->href);
+	$one_dom=str_get_html($one);
+	$cost=$one_dom->find('.item_current_price',0);
+	$tobd['price']=(int)$cost->plaintext;
+//$sql->Insert('Tables',$tobd);
+	
+}	
