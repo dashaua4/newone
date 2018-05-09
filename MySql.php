4e1298899cee4c4e3dca49a56e240a4684d7a6ba
dashaua4 <dashaua4@users.noreply.github.com>
2018-05-09 18:49:42 +0300

    Create MySql.php

---
+++ MySql.php
@@ -0,0 +1,60 @@
+class SQL
+{
+ private static $instance;
+private $db;
+public static function Instance()
+{if(self::$instance==null)
+ {self::$instance=new SQL();}
+ return self::$instance;
+} 
+
+private function _construct()
+{ setlocale(LC_ALL,'ru_RU.UTF8');
+ $this->db=new PDO('mysql:host=localhost;dbname=DiplomBD','root','');
+ $this->db->exec('SET NAMES UTF8');
+ $this->db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+ 
+}
+public function Select($query)
+{ $q=$this->db->prepare($query);
+ $q->execute();
+ if($q->errorCode()!=PDO::ERR_NONE)
+ {$info=$q->errorInfo();echo($info[2]);}
+return $q->fetchAll();
+}
+public function Insert($table,$object)
+{ $this->db=new PDO('mysql:host=localhost;dbname=DiplomBD','root','');
+ 
+$columns =array();
+foreach($object as $key=>$value)
+{
+ $columns[]=$key;
+ $masks[]=":$key";
+ if($value==null)
+ {$object[$key]='NULL';}
+}
+ $columns_s=implode(',',$columns);
+ $masks_s=implode(',',$masks);
+ 
+ $query="INSERT INTO $table ($columns_s) VALUE ($masks_s)";
+ $q=$this->db->prepare($query);
+ $q->execute($object);
+ if($q->errorCode()!=PDO::ERR_NONE)
+ {$info=$q->errorInfo();echo($info[2]);}
+
+
+$result= $this->db->lastInsertId();
+ return $result;
+ 
+ 
+} 
+ 
+}
+      
+            
+            
+            
+            
+            
+            
+?>
