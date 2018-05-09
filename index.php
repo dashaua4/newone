387a5bfbc465a05f6ffb635e9f8e8df43c21eed0
dashaua4 <dashaua4@users.noreply.github.com>
2018-05-09 22:27:17 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,33 +1,28 @@
-<html>
-	<head>
-	</head>
-	<boby>
-		<h1>HELLO WORLD11111</h1>
-	</body>	
-</html>	
-
 
 <?php
+$servername = "diplomdb-mysqldbserver.mysql.database.azure.com";
+$username = "diplomadmin";
+$password = "Alexandra11";
+$dbname = "mysqldatabase44500";
 
-include_once('curl_query.php');
-include_once('simple_html_dom.php');
-include_once('SQL.php');
-$html=curl_get('https://meblihit.com.ua/catalog/modul%60na_systema_ofys/');
-
-$sql=SQL::Instance();
-$dom=str_get_html($html);
-$tables=$dom->find('.name_product');
-foreach($tables as $table)
-{ 
-
-$tobd=array();
-$a=$table->find('a',0);
-
-	$tobd['name']=$a->plaintext;
-	$one=curl_get('https://meblihit.com.ua'.$a->href);
-	$one_dom=str_get_html($one);
-	$cost=$one_dom->find('.item_current_price',0);
-	$sql->createTable();
-//$sql->Insert('Tables',$tobd);
-	
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
+
+$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "id: " . $row["id"]. " - name: " . $row["name"]. " " . $row["price"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+mysqli_close($conn);
+?>
