640b87cd3521f875eedfdd7fb90a00a6a97a300b
dashaua4 <dashaua4@users.noreply.github.com>
2018-05-09 21:23:58 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,37 +1,3 @@
-<html>
-	<head>
-	</head>
-	<boby>
-		<h1>HELLO WORLD11111</h1>
-	</body>	
-</html>	
-
-
-<?php
-
-include_once('curl_query.php');
-include_once('simple_html_dom.php');
-include_once('SQL.php');
-$html=curl_get('https://meblihit.com.ua/catalog/modul%60na_systema_ofys/');
-
-$sql=SQL::Instance();
-$dom=str_get_html($html);
-$tables=$dom->find('.name_product');
-foreach($tables as $table)
-{ 
-
-$tobd=array();
-$a=$table->find('a',0);
-
-	$tobd['name']=$a->plaintext;
-	$one=curl_get('https://meblihit.com.ua'.$a->href);
-	$one_dom=str_get_html($one);
-	$cost=$one_dom->find('.item_current_price',0);
-	$tobd['price']=(int)$cost->plaintext;
-//	$sql->createTable();
-//$sql->Insert('Tables',$tobd);
-	
-}	
 <!DOCTYPE html>
 <html>
 <body>
