de3db959397dc8f567b1550030770c9a57aa96d7
dashaua4 <dashaua4@users.noreply.github.com>
2018-05-10 12:24:04 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,5 @@
 <?php
 
-$servername = "diplomdb-mysqldbserver.mysql.database.azure.com";
-$username = "diplomadmin@diplomdb-mysqldbserver";
-$password = "Alexandra11";
-$dbname = "mysqldatabase44500";
-echo "GGGGGGGGGG";
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-$sql = "SELECT id, name, price FROM Locker";
-$result = mysqli_query($conn, $sql);
-if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-    while($row = mysqli_fetch_assoc($result)) {
-        echo "id: " . $row["id"]. " - name: " . $row["name"]. " " . $row["price"]. "<br>";
-    }
-} else {
-    echo "0 results";
-}
-
-mysqli_close($conn);
 
 if ((include 'SQL.php') == TRUE) {
     echo 'OK';
