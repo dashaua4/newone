f217b62a26f32987242da1fe711c09ae6270aa0d
dashaua4 <dashaua4@users.noreply.github.com>
2018-05-10 12:54:30 +0300

    Update SQL.php

--- SQL.php
+++ SQL.php
@@ -1,29 +1,6 @@
 <?php
-class SQL
-{ 
- private static $instance;
-private $db;
-public static function Instance()
-{if(self::$instance==null)
- {self::$instance=new SQL();}
- return self::$instance;
-} 
 
-private function _construct()
-{ setlocale(LC_ALL,'ru_RU.UTF8');
- $this->db=new PDO('mysql:host=diplomdb-mysqldbserver.mysql.database.azure.com;port=3306;dbname=mysqldatabase44500','diplomadmin@diplomdb-mysqldbserver','Alexandra11');
- $this->db->exec('SET NAMES UTF8');
- $this->db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
- 
-}
-public function Select($query)
-{ $q=$this->db->prepare($query);
- $q->execute();
- if($q->errorCode()!=PDO::ERR_NONE)
- {$info=$q->errorInfo();echo($info[2]);}
-return $q->fetchAll();
-}
-public function Insert($table,$object)
+function Insert($table,$object)
 { 
  $servername = "diplomdb-mysqldbserver.mysql.database.azure.com";
 $username = "diplomadmin@diplomdb-mysqldbserver";
@@ -63,22 +40,5 @@ $result= $this->conn->lastInsertId();
  
  
 } 
-public function createTable()
-{ $this->db=new PDO('mysql:host=diplomdb-mysqldbserver.mysql.database.azure.com;port=3306;dbname=mysqldatabase44500','diplomadmin@diplomdb-mysqldbserver','Alexandra11');
- 
-  $sqlList = ['CREATE TABLE IF NOT EXISTS Chair (
-                        id  PRIMARY KEY,
-                        name  varchar(64) NOT NULL UNIQUE,
-                        price  int(255) NOT NULL UNIQUE 
-                     );'];
-foreach ($sqlList as $sql) {
-            $this->db->exec($sql);
-        }
-        
-        return $this;
- 
-}
-echo "loooool";
 
-            
 ?>
