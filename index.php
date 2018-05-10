9c1f92d57bcd0c67ec2f66a32b1cffbc8b5165f8
dashaua4 <dashaua4@users.noreply.github.com>
2018-05-10 12:59:00 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,23 +2,7 @@
 
 if((include('SQL.php'))==TRUE)
 {echo "OK";}
-$html=curl_get('https://meblihit.com.ua/catalog/modul%60na_systema_ofys/');
 
-
-$dom=str_get_html($html);
-$tables=$dom->find('.name_product');
-foreach($tables as $table)
-{ 
-
-$tobd=array();
-$a=$table->find('a',0);
-
-	$tobd['name']=$a->plaintext;
-	$one=curl_get('https://meblihit.com.ua'.$a->href);
-	$one_dom=str_get_html($one);
-	$cost=$one_dom->find('.item_current_price',0);
-	$tobd['price']=(int)$cost->plaintext;
-//Insert('Tables',$tobd);
 $servername = "diplomdb-mysqldbserver.mysql.database.azure.com";
 $username = "diplomadmin@diplomdb-mysqldbserver";
 $password = "Alexandra11";
@@ -30,7 +14,7 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-$sql = "SELECT id, name, price FROM tables";
+$sql = "SELECT id, name, price FROM Tables";
 $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
