21f749ba02c41cd6eb24091f6ae8673236c1dd95
dashaua4 <dashaua4@users.noreply.github.com>
2018-05-10 11:39:04 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
-
-include 'SQL.php';
+include_once('curl_query.php');
+include_once('simple_html_dom.php');
+include_once('SQL.php');
 $sql11=SQL::Instance();
 
 $html=curl_get('https://meblihit.com.ua/catalog/modul%60na_systema_ofys/');
@@ -23,7 +24,29 @@ $a=$table->find('a',0);
 $sql11->Insert('Tables',$tobd);
 	
 }	
+$servername = "diplomdb-mysqldbserver.mysql.database.azure.com";
+$username = "diplomadmin@diplomdb-mysqldbserver";
+$password = "Alexandra11";
+$dbname = "mysqldatabase44500";
+echo "sdfvgbhnjimk,lkjgf";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+$sql = "SELECT id, name, price FROM Locker";
+$result = mysqli_query($conn, $sql);
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "id: " . $row["id"]. " - name: " . $row["name"]. " " . $row["price"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
 
+mysqli_close($conn);
 
 
 
