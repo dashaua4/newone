863bd2e24da3720e37a634aba2f693a66e49accf
dashaua4 <dashaua4@users.noreply.github.com>
2018-05-10 10:22:31 +0300

    Update index.php

--- index.php
+++ index.php
@@ -34,17 +34,6 @@ $sql->Insert('Tables',$tobd);
 	
 }
 
-$sql = "SELECT id, name, price FROM Tables";
-$result = mysqli_query($conn, $sql);
-
-if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-    while($row = mysqli_fetch_assoc($result)) {
-        echo "id: " . $row["id"]. " - name: " . $row["name"]. " " . $row["price"]. "<br>";
-    }
-} else {
-    echo "0 results";
-}
 
 mysqli_close($conn);
 
