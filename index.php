881aaf83602010c7b4326f2a6ee6d42ffb491082
dashaua4 <dashaua4@users.noreply.github.com>
2018-05-10 11:40:36 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,5 @@
 <?php
-$sql11=SQL::Instance();
 
-$html=curl_get('https://meblihit.com.ua/catalog/modul%60na_systema_ofys/');
-
-
-$dom=str_get_html($html);
-$tables=$dom->find('.name_product');
-foreach($tables as $table)
-{ 
-
-$tobd=array();
-$a=$table->find('a',0);
-
-	$tobd['name']=$a->plaintext;
-	$one=curl_get('https://meblihit.com.ua'.$a->href);
-	$one_dom=str_get_html($one);
-	$cost=$one_dom->find('.item_current_price',0);
-	$tobd['price']=(int)$cost->plaintext;
-    echo $tobd;
-//$sql11->Insert('Tables',$tobd);
-	
-}	
 $servername = "diplomdb-mysqldbserver.mysql.database.azure.com";
 $username = "diplomadmin@diplomdb-mysqldbserver";
 $password = "Alexandra11";
