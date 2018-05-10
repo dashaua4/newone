fc3c524eea13f116235b4fb08826e255a88ef1a2
dashaua4 <dashaua4@users.noreply.github.com>
2018-05-10 10:53:50 +0300

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-$sql = "SELECT id, name, price FROM Tables";
+$sql = "SELECT id, name, price FROM Locker";
 $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
