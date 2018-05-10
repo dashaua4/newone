b629d2f386d0121047eeeb57d20147ef6b70914c
dashaua4 <dashaua4@users.noreply.github.com>
2018-05-10 14:07:23 +0300

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $servername = "diplomdb-mysqldbserver.mysql.database.azure.com";
 $username = "diplomadmin@diplomdb-mysqldbserver";
 $password = "Alexandra11";
 $dbname = "mysqldatabase44500";
-echo "Hello World";
+
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
