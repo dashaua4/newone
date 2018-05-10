8c93797109ec40927b3bb2a3e7c71d487ec4bf9c
dashaua4 <dashaua4@users.noreply.github.com>
2018-05-10 10:03:27 +0300

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,25 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
+$html=curl_get('https://meblihit.com.ua/catalog/modul%60na_systema_ofys/');
 
+$sql=SQL::Instance();
+$dom=str_get_html($html);
+$tables=$dom->find('.name_product');
+foreach($tables as $table)
+{ 
+
+$tobd=array();
+$a=$table->find('a',0);
+
+	$tobd['name']=$a->plaintext;
+	$one=curl_get('https://meblihit.com.ua'.$a->href);
+	$one_dom=str_get_html($one);
+	$cost=$one_dom->find('.item_current_price',0);
+	$tobd['price']=(int)$cost->plaintext;
+//$sql->Insert('Tables',$tobd);
+	
+}
 
 
 
