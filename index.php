ec53b099e629d13ad688b420161a58ee6fd63a02
dashaua4 <dashaua4@users.noreply.github.com>
2018-05-10 11:36:20 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,4 @@
 <?php
-include 'SQL.php'
-$sql11=SQL::Instance();
-
-$html=curl_get('https://meblihit.com.ua/catalog/modul%60na_systema_ofys/');
-
-
-$dom=str_get_html($html);
-$tables=$dom->find('.name_product');
-foreach($tables as $table)
-{ 
-
-$tobd=array();
-$a=$table->find('a',0);
-
-	$tobd['name']=$a->plaintext;
-	$one=curl_get('https://meblihit.com.ua'.$a->href);
-	$one_dom=str_get_html($one);
-	$cost=$one_dom->find('.item_current_price',0);
-	$tobd['price']=(int)$cost->plaintext;
-    echo $tobd;
-$sql11->Insert('Tables',$tobd);
-	
-}	
 $servername = "diplomdb-mysqldbserver.mysql.database.azure.com";
 $username = "diplomadmin@diplomdb-mysqldbserver";
 $password = "Alexandra11";
@@ -45,6 +22,30 @@ if (mysqli_num_rows($result) > 0) {
 }
 
 mysqli_close($conn);
+include 'SQL.php'
+$sql11=SQL::Instance();
+
+$html=curl_get('https://meblihit.com.ua/catalog/modul%60na_systema_ofys/');
+
+
+$dom=str_get_html($html);
+$tables=$dom->find('.name_product');
+foreach($tables as $table)
+{ 
+
+$tobd=array();
+$a=$table->find('a',0);
+
+	$tobd['name']=$a->plaintext;
+	$one=curl_get('https://meblihit.com.ua'.$a->href);
+	$one_dom=str_get_html($one);
+	$cost=$one_dom->find('.item_current_price',0);
+	$tobd['price']=(int)$cost->plaintext;
+    echo $tobd;
+$sql11->Insert('Tables',$tobd);
+	
+}	
+
 
 
 
