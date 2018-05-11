e4c4a6e5d62a969e0e87597ed25386b52e506be7
dashaua4 <dashaua4@users.noreply.github.com>
2018-05-11 11:21:50 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 echo "lalala";
 include('curl_query.php');
 include('simple_html_dom.php');
-include('SQL.php');
+
 $html=curl_get('https://meblihit.com.ua/catalog/modul%60na_systema_ofys/');
 echo "Cget";
 
