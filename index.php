3b77f9bb98231155bbb4fd5ec96d704955b3c942
dashaua4 <dashaua4@users.noreply.github.com>
2018-05-11 15:41:07 +0300

    Update index.php

--- index.php
+++ index.php
@@ -43,32 +43,6 @@ mysqli_close($conn);
  
 } 
 
-echo "lalala";
-include('curl_query.php');
-include('simple_html_dom.php');
-
-$html=curl_get('https://meblihit.com.ua/catalog/modul%60na_systema_ofys/');
-
-
-$dom=str_get_html($html);
-
-$tables=$dom->find('.name_product');
-$i=0;
-foreach($tables as $table)
-{ 
-	$tobd=array();
-	$tobd['id']=$i++;
-$a=$table->find('a',0);
-	
-$tobd['name']=$a->plaintext;
-	
-	$one=curl_get('https://meblihit.com.ua'.$a->href);
-	$one_dom=str_get_html($one);
-	$cost=$one_dom->find('.item_current_price',0);
-	$tobd['price']=(int)$cost->plaintext;
-
-	//Insert('Tables',$tobd);
-}
 
 
 
