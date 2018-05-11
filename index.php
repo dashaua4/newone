5837b19ccd238163bd732fbdc15db2e453ec6d35
dashaua4 <dashaua4@users.noreply.github.com>
2018-05-11 10:54:04 +0300

    Update index.php

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ $a=$table->find('a',0);
 	$one_dom=str_get_html($one);
 	$cost=$one_dom->find('.item_current_price',0);
 	$tobd['price']=(int)$cost->plaintext;
-//Insert('Tables',$tobd);
+NEW();//Insert('Tables',$tobd);
 	
 }
 
