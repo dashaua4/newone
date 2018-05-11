2e59515c30962eb43542cd11b89100a47d5bb21c
dashaua4 <dashaua4@users.noreply.github.com>
2018-05-11 20:14:06 +0300

    Update index.php

--- index.php
+++ index.php
@@ -64,20 +64,21 @@ $dom=str_get_html($html);
 $tables=$dom->find('.name_product');
 $i=1;
 //echo $tables;
-
+foreach($tables as $table)
+{
 $tobd=array();
 	$tobd['id']=$i;
 $a=$table->find('a',0);
 	
 $tobd['name']="'".$a->plaintext."'";
-echo $a->plaintext;
+//echo $a->plaintext;
 	$one=curl_get('https://meblihit.com.ua'.$a->href);
 
 	$one_dom=str_get_html($one);
 	$cost=$one_dom->find('.item_current_price',0);
 //echo $cost;
 	$tobd['price']=(int)$cost->plaintext;
-
+}
 	//Insert('Tables',$tobd);
 
 
