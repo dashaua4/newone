8ecbe88996a0ce30c52d7c4cd5ac1c3a5807c882
dashaua4 <dashaua4@users.noreply.github.com>
2018-05-11 11:16:01 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,12 +5,12 @@ echo "lalala";
 include('curl_query.php');
 
 
-$html=curl_get('http://mebli-lviv.com.ua/ru/pysmovi-kompyuterni-stoly1/');
+$html=curl_get('https://meblihit.com.ua/catalog/modul%60na_systema_ofys/');
 echo "Cget";
 
 $dom=str_get_html($html);
 echo "strgethtml";
-$tables=$dom->find('.name');
+$tables=$dom->find('.name_product');
 echo "get table";
 foreach($tables as $table)
 { 
