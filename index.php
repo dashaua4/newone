43bcee205db606689336bd6dd2e95663a56a04d2
dashaua4 <dashaua4@users.noreply.github.com>
2018-05-11 20:35:37 +0300

    Update index.php

--- index.php
+++ index.php
@@ -79,8 +79,12 @@ $tobd['name']="'".$a->plaintext."'";
 //echo $cost;
 	$tobd['price']=(int)$cost->plaintext;
 }
-	Insert('Tables',$tobd);
-
+	//Insert('Tables',$tobd);
+foreach ($tobd as $key => $value) {
+    // $arr[3] будет перезаписываться значениями $arr при каждой итерации цикла
+    echo "{$key} => {$value} ";
+    print_r($tobd).'<br>';
+}
 
 $servername = "diplomdb-mysqldbserver.mysql.database.azure.com";
 $username = "diplomadmin@diplomdb-mysqldbserver";
