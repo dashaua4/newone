8815ce905c5700ee7055c1da31c9804e732aca85
dashaua4 <dashaua4@users.noreply.github.com>
2018-05-11 11:26:02 +0300

    Update index.php

--- index.php
+++ index.php
@@ -17,37 +17,10 @@ foreach($tables as $table)
 	$tobd=array();
 $a=$table->find('a',0);
 
-	$tobd['name']=$a->plaintext;
-	$one=curl_get('https://meblihit.com.ua'.$a->href);
-	$one_dom=str_get_html($one);
-	$cost=$one_dom->find('.item_current_price',0);
-	$tobd['price']=(int)$cost->plaintext;
-//Insert('Tables',$tobd);
+	echo "NO";
 
 	
 }
-$servername = "diplomdb-mysqldbserver.mysql.database.azure.com";
-$username = "diplomadmin@diplomdb-mysqldbserver";
-$password = "Alexandra11";
-$dbname = "mysqldatabase44500";
-echo "GGGGGGGGGG";
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-$sql = "SELECT id, name, price FROM Tables";
-$result = mysqli_query($conn, $sql);
-if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-    while($row = mysqli_fetch_assoc($result)) {
-        echo "id: " . $row["id"]. " - name: " . $row["name"]. " " . $row["price"]. "<br>";
-    }
-} else {
-    echo "0 results";
-}
 
-mysqli_close($conn);
 
 ?>
