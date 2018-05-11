eb58c69ab7c103eb1bcbb86f0cfade48942bea13
dashaua4 <dashaua4@users.noreply.github.com>
2018-05-11 10:55:00 +0300

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ if((include('SQL.php'))==TRUE)
 $html=curl_get('https://meblihit.com.ua/catalog/modul%60na_systema_ofys/');
 
 $dom=str_get_html($html);
+echo $dom;
 $tables=$dom->find('.name_product');
 foreach($tables as $table)
 { 
@@ -20,7 +21,7 @@ $a=$table->find('a',0);
 	$one_dom=str_get_html($one);
 	$cost=$one_dom->find('.item_current_price',0);
 	$tobd['price']=(int)$cost->plaintext;
-NEW();//Insert('Tables',$tobd);
+//NEW();//Insert('Tables',$tobd);
 	
 }
 
