1c2e58962872cceb2dbbc8ba1be3b3a59c4a257d
dashaua4 <dashaua4@users.noreply.github.com>
2018-05-11 21:07:28 +0300

    Update index.php

--- index.php
+++ index.php
@@ -96,15 +96,11 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-$sql = "SELECT id, name, price FROM Tables";
-$result = mysqli_query($conn, $sql);
-if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-    while($row = mysqli_fetch_assoc($result)) {
-        echo "id: " . $row["id"]. " - name: " . $row["name"]. " " . $row["price"]. "<br>";
-    }
+$sql = "DROP TABLE Tables";
+if (mysqli_query($conn, $sql)) {
+echo "New record created successfully";
 } else {
-    echo "0 results";
+echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
 
 mysqli_close($conn);
