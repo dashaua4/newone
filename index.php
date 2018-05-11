8cba439dbdfd083a5023cdcac55845ce6a9d7e70
dashaua4 <dashaua4@users.noreply.github.com>
2018-05-11 11:14:57 +0300

    Update index.php

--- index.php
+++ index.php
@@ -6,9 +6,12 @@ include('curl_query.php');
 
 
 $html=curl_get('http://mebli-lviv.com.ua/ru/pysmovi-kompyuterni-stoly1/');
-$dom=str_get_html($html);
+echo "Cget";
 
+$dom=str_get_html($html);
+echo "strgethtml";
 $tables=$dom->find('.name');
+echo "get table";
 foreach($tables as $table)
 { 
 	echo "NO";
