fb460881164f317374cd3a66624ce65dd6d653cd
dashaua4 <dashaua4@users.noreply.github.com>
2018-05-11 12:58:05 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,31 @@
 <?php
-function Insert()
+function Insert($table,$object)
 { 
 	$servername = "diplomdb-mysqldbserver.mysql.database.azure.com";
 $username = "diplomadmin@diplomdb-mysqldbserver";
 $password = "Alexandra11";
 $dbname = "mysqldatabase44500";
-echo "GGGGGGGGGG";
+
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
-//mysqli_close($conn);
- echo "GFGHJFGFHJK";
- //$this->db=new PDO('mysql:host=diplomdb-mysqldbserver.mysql.database.azure.com;port=3306;dbname=mysqldatabase44500','diplomadmin@diplomdb-mysqldbserver','Alexandra11');
+$columns =array();
+foreach($object as $key=>$value)
+{
+ $columns[]=$key;
+ $masks[]=":$key";
+ if($value==null)
+ {$object[$key]='NULL';}
+}
+ $columns_s=implode(',',$columns);
+ $masks_s=implode(',',$masks);
  
- $sql="INSERT INTO Tables (id,name,price) VALUE (1,'Модульна система Офис (ofіs)',13991)";
+
+ 
+ $sql="INSERT INTO $table ($columns_s) VALUE ($masks_s)";
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
 } else {
@@ -34,12 +42,11 @@ include('curl_query.php');
 include('simple_html_dom.php');
 
 $html=curl_get('https://meblihit.com.ua/catalog/modul%60na_systema_ofys/');
-echo "Cget";
+
 
 $dom=str_get_html($html);
-echo "strgethtml";
+
 $tables=$dom->find('.name_product');
-echo "get table";
 
 foreach($tables as $table)
 { 
@@ -51,33 +58,10 @@ $tobd['name']=$a->plaintext;
 	$one_dom=str_get_html($one);
 	$cost=$one_dom->find('.item_current_price',0);
 	$tobd['price']=(int)$cost->plaintext;
-	echo (int)$cost->plaintext;
-	
-}
-//Insert();
-$servername = "diplomdb-mysqldbserver.mysql.database.azure.com";
-$username = "diplomadmin@diplomdb-mysqldbserver";
-$password = "Alexandra11";
-$dbname = "mysqldatabase44500";
-echo "GGGGGGGGGG";
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-$sql = "SELECT id, name, price FROM Tables";
-$result = mysqli_query($conn, $sql);
-if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-    while($row = mysqli_fetch_assoc($result)) {
-        echo "id: " . $row["id"]. " - name: " . $row["name"]. " " . $row["price"]. "<br>";
-    }
-} else {
-    echo "0 results";
+
+	Insert('Tables',$tobd);
 }
 
-mysqli_close($conn);
 
 
 ?>
