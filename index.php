b81b3fe30f1aa3f20860b4993c224dfc1908ea62
dashaua4 <dashaua4@users.noreply.github.com>
2018-05-11 13:21:16 +0300

    Update index.php

--- index.php
+++ index.php
@@ -59,7 +59,7 @@ $tobd['name']=$a->plaintext;
 	$cost=$one_dom->find('.item_current_price',0);
 	$tobd['price']=(int)$cost->plaintext;
 
-	Insert('Tables',$tobd);
+	echo(Insert('Tables',$tobd));
 }
 
 
