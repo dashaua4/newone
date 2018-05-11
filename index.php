6ca30a9294d4480baa62c86c0630d1389a0b1731
dashaua4 <dashaua4@users.noreply.github.com>
2018-05-11 11:29:31 +0300

    Update index.php

--- index.php
+++ index.php
@@ -17,6 +17,7 @@ foreach($tables as $table)
 	$tobd=array();
 $a=$table->find('a',0);
 $tobd['name']=$a->plaintext;
+	echo $a->plaintext;
 	$one=curl_get('https://meblihit.com.ua'.$a->href);
 	$one_dom=str_get_html($one);
 	echo "NO";
