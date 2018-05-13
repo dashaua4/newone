0b4033251f54d05428d48e58c41f2ed4ed3d3d72
dashaua4 <dashaua4@users.noreply.github.com>
2018-05-13 15:01:52 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,29 @@
  <?php
+function CR_Table()
+{$servername = "diplomdb-mysqldbserver.mysql.database.azure.com";
+$username = "diplomadmin@diplomdb-mysqldbserver";
+$password = "Alexandra11";
+$dbname = "mysqldatabase44500";
+echo "GGGGGGGGGG";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+$sql = "CREATE TABLE Tables (
+id INTEGER AUTO_INCREMENT PRIMARY KEY, 
+name VARCHAR(64) ,
+price INTEGER
+)";
+if ($conn->query($sql) === TRUE) {
+    echo "Table Table created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+mysqli_close($conn);
+}
 function Insert($table,$object)
 { 
 	
@@ -36,6 +61,32 @@ if ($conn->multi_query($sql) === TRUE) {
 mysqli_close($conn);
 
 } 
+function Select()
+{
+	$servername = "diplomdb-mysqldbserver.mysql.database.azure.com";
+$username = "diplomadmin@diplomdb-mysqldbserver";
+$password = "Alexandra11";
+$dbname = "mysqldatabase44500";
+echo "GGGGGGGGGG";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+$sql = "SELECT id, name, price FROM Tables";
+$result = mysqli_query($conn, $sql);
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "id: " . $row["id"]. " - name: " . $row["name"]. " " . $row["price"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+mysqli_close($conn);
+}
 
 echo "lalala";
 include('curl_query.php');
@@ -55,35 +106,7 @@ $tobd=array();
 	$one_dom=str_get_html($one);
 	$cost=$one_dom->find('.item_current_price',0);
 	$tobd['price']=(int)$cost->plaintext;
-	//Insert('Tables',$tobd);
+	Insert('Tables',$tobd);
 }
-
-$servername = "diplomdb-mysqldbserver.mysql.database.azure.com";
-$username = "diplomadmin@diplomdb-mysqldbserver";
-$password = "Alexandra11";
-$dbname = "mysqldatabase44500";
-echo "GGGGGGGGGG";
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-$sql = "CREATE TABLE Tables (
-id INTEGER AUTO_INCREMENT PRIMARY KEY, 
-name VARCHAR(64) ,
-price INTEGER
-)";
-if ($conn->query($sql) === TRUE) {
-    echo "Table Table created successfully";
-} else {
-    echo "Error creating table: " . $conn->error;
-}
-
-mysqli_close($conn);
-
-
-
-
-
+Select();
 ?>
