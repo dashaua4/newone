23c2187ba6c596b1ef4b167472a55846c6b0ac08
dashaua4 <dashaua4@users.noreply.github.com>
2018-05-13 14:24:03 +0300

    Update index.php

--- index.php
+++ index.php
@@ -91,18 +91,15 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-$sql = "SELECT id, name, price FROM Tables";
-$result = mysqli_query($conn, $sql);
-if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-    while($row = mysqli_fetch_assoc($result)) {
-        echo "id: " . $row["id"]. " - name: " . $row["name"]. " " . $row["price"]. "<br>";
-    }
+$sql = "DELETE FROM TAbles WHERE id=2";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Record deleted successfully";
 } else {
-    echo "0 results";
+    echo "Error deleting record: " . $conn->error;
 }
 
-mysqli_close($conn);
+$conn->close();
 
 
 
