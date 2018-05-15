01e373bbfb9614a1a8d1a86a053d67fc1d9ad16e
dashaua4 <dashaua4@users.noreply.github.com>
2018-05-16 00:18:22 +0300

    Update index.php

--- index.php
+++ index.php
@@ -88,7 +88,7 @@ mysqli_close($conn);
 } 
 
 
-echo "lalala";
+
 include('curl_query.php');
 include('simple_html_dom.php');
 //Drop_table();
@@ -108,7 +108,7 @@ $tobd=array();
 	$one_dom=str_get_html($one);
 	$cost=$one_dom->find('.item_current_price',0);
 	$tobd['price']=(int)$cost->plaintext;
-	Insert('Chairs',$tobd);
+	//Insert('Chairs',$tobd);
 }
 
 
@@ -116,19 +116,19 @@ $servername = "diplomdb-mysqldbserver.mysql.database.azure.com";
 $username = "diplomadmin@diplomdb-mysqldbserver";
 $password = "Alexandra11";
 $dbname = "mysqldatabase44500";
-echo "GGGGGGGGGG";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-$sql = "SELECT id, name, price FROM Chairs ";
+echo "Table 'Tables'";
+$sql = "SELECT id, name, price FROM Tables ";
 $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
-         echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . $row["price"]. "<br>";
+         echo "id: " . $row["id"]. " - Name: " . $row["name"]. "-" . $row["price"]. "<br>";
     }
 } else {
     echo "0 results";
