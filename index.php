f5569d96146dbcd85162babefb15db347a7a69c6
dashaua4 <dashaua4@users.noreply.github.com>
2018-05-15 13:22:40 +0300

    Update index.php

--- index.php
+++ index.php
@@ -102,7 +102,7 @@ $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
-        echo "id: " . $row["id"]. " - name: " . $row["name"]. $row["price"];
+         echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . $row["price"]. "<br>";
     }
 } else {
     echo "0 results";
