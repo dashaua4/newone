5794644148ed139f732ebca1cdce5d9a12fe34cf
dashaua4 <dashaua4@users.noreply.github.com>
2018-05-15 13:20:04 +0300

    Update index.php

--- index.php
+++ index.php
@@ -68,5 +68,29 @@ mysqli_close($conn);
 
 echo "lalala";
 
+$servername = "diplomdb-mysqldbserver.mysql.database.azure.com";
+$username = "diplomadmin@diplomdb-mysqldbserver";
+$password = "Alexandra11";
+$dbname = "mysqldatabase44500";
+echo "GGGGGGGGGG";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+$sql = "SELECT id, name, price FROM Tables ";
+$result = mysqli_query($conn, $sql);
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "id: " . $row["id"]. " - name: " . $row["name"];
+    }
+} else {
+    echo "0 results";
+}
+
+mysqli_close($conn);
+//Cr_table();
 
 ?>
