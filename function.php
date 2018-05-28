fe875675be5b5d62be2f10af59d5cad5de62cb80
dashaua4 <dashaua4@users.noreply.github.com>
2018-05-28 15:27:35 +0300

    Update function.php

--- function.php
+++ function.php
@@ -11,7 +11,6 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-echo "Table 'Tables'"."<br>";
 $sql ="SELECT MAX(price) as price FROM Tables";
 $result = mysqli_query($conn, $sql);
 $date=mysqli_fetch_array($result);
