a42bc60f3e509d064bc3920f6f0ce90a31788727
dashaua4 <dashaua4@users.noreply.github.com>
2018-05-28 12:49:35 +0300

    Update function.php

--- function.php
+++ function.php
@@ -12,12 +12,12 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 echo "Table 'Tables'"."<br>";
-$sql = "SELECT MAX(price) FROM Tables";
+$sql = "SELECT id,name,price FROM Tables WHERE price>5000";
 $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
-         echo $row["price"]. "<br>";
+         echo "id: " . $row["id"]. " - Name: " . $row["name"]. "-" .$row["price"]. "<br>";
     }
 } else {
     echo "0 results";
