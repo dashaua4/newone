73136707e6be9a70c4a52f0b34287f7b73f83072
dashaua4 <dashaua4@users.noreply.github.com>
2018-05-29 11:07:15 +0300

    Update and rename menu.html to menu.php

---
+++ menu.php
@@ -0,0 +1,30 @@
+
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title></title>
+ <link href="style.css" rel="stylesheet" />
+
+</head>
+
+<body  >
+<div class="m">
+	
+		<nav>
+  <ul class="topmenu">
+    <li><a target="cont" href="first.php" data="Головна" class="active">Головна<span class="fa fa-angle-down"></span></a> </li>
+    <li><a target="cont" href="info.php" data="Інформація">Бізнес-планування</a></li>
+    <li><a href="">Функції</a>
+	
+   <ul class="submenu">
+        <li><a target="cont" href="b_function.php" data="Функції">Створити бізнес-план</a>
+        </li>
+    
+      </ul>	</li>
+    <li><a target="cont" href="contacts.php" data="Контакти">Контакти</a></li>
+  </ul>
+</nav>
+		</div>
+</div>
+</body>
+</html> 
