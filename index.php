f4aa14fd502d37b9cc580a5dd454eab97dfd94ca
dashaua4 <dashaua4@users.noreply.github.com>
2018-05-29 12:13:57 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 
 include('curl_query.php');
 include('simple_html_dom.php');
-include('main.php');
+//include('main.php');
 include('function.php');
 //Drop_table();
 //Cr_table();
@@ -27,9 +27,7 @@ $tobd=array();
 	//Insert('Chairs',$tobd);
 	
 }
+$name=SelectT('Tables');
 
-
-//SelectT('Tables');
-
-
+echo $name;
 ?>
