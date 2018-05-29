21eaac28c078c7a78a7a07bb159a645dd7b833ae
dashaua4 <dashaua4@users.noreply.github.com>
2018-05-29 11:00:29 +0300

    Update main.php

--- main.php
+++ main.php
@@ -6,15 +6,15 @@
  </head>
  <frameset rows="20%,80%,5%" bordercolor="grey" border="0" >
   <frameset>
-    <frame src="menu.html" name="footer" frameborder="1" scrolling="no" noresize></frame>
+    <frame src="menu.php" name="footer" frameborder="1" scrolling="no" noresize></frame>
 	</frameset>
    <frameset cols="15%,85%">
-   <frame src="left.html" frameborder="0" name="left" scrolling="no" noresize></frame>
-    <frame src="main.html"  frameborder="0" name="cont" scrolling="yes" noresize></frame>
+   <frame src="left.php" frameborder="0" name="left" scrolling="no" noresize></frame>
+    <frame src="main.php"  frameborder="0" name="cont" scrolling="yes" noresize></frame>
 	
    </frameset>
    <frameset>
-    <frame src="footer.html" name="footer" frameborder="1" scrolling="no" noresize></frame>
+    <frame src="footer.php" name="footer" frameborder="1" scrolling="no" noresize></frame>
 	</frameset>
  </frameset>
 <body>
