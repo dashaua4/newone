3666e26a41c5adaeed6e22c009dbe9d71d06d469
dashaua4 <dashaua4@users.noreply.github.com>
2018-06-04 08:20:42 +0300

    Update index.php

--- index.php
+++ index.php
@@ -9,9 +9,10 @@ include('simple_html_dom.php');
 include('function.php');
 //Drop_table();
 //Cr_table();
-$html=curl_get('https://hard.rozetka.com.ua/computers/c80095/filter/70553=432604/');
+echo 'GGGGGGGG';
+$html=curl_get('https://deshevshe.net.ua/desktop/');
 $dom=str_get_html($html);
-$tables=$dom->find('.g-i-tile-i-title clearfix');
+$tables=$dom->find('.product_title');
 $i=1;
 foreach($tables as $table)
 {
@@ -19,11 +20,11 @@ $tobd=array();
 	$tobd['id']=$i++;
 	$a=$table->find('a',0);
 	$tobd['name']="'".$a->plaintext."'";
-	$one=curl_get('https://hard.rozetka.com.ua'.$a->href);
+	$one=curl_get('https://deshevshe.net.ua'.$a->href);
 
 
 	$one_dom=str_get_html($one);
-	$cost=$one_dom->find('.detail-price-uah',0);
+	$cost=$one_dom->find('.product_price_new',0);
 	$tobd['price']=(int)$cost->plaintext;
 	
 	//Insert('Chairs',$tobd);
