bb85e4e70640dced8b4a45a97095c6bf057fa0f8
dashaua4 <dashaua4@users.noreply.github.com>
2018-06-04 12:11:46 +0300

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,8 @@ include('curl_query.php');
 include('simple_html_dom.php');
 //include('main.php');
 include('function.php');
-//Drop_table();
-//Cr_table();
+Drop_table();
+Cr_table();
 //echo 'GGGGGGGG';
 $html=curl_get('https://deshevshe.net.ua/desktop/filters/presence=1;videokarta=integrirovannaya_3470;max=7000');
 $dom=str_get_html($html);
@@ -29,12 +29,12 @@ $n=$tobd['name'];
 		
 
 	$one_dom=str_get_html($one);
-//	echo $one;
+	echo $one;
 	$cost=$one_dom->find('.product__price_current',0);
 	$tobd['price']=(int)$cost->plaintext;
 	$p=$tobd['price'];
 //	echo $n.'-'.$p.'<br>';
-	//Insert('WG_system',$tobd);
+	Insert('WG_system',$tobd);
 	
 }
 SelectT('WG_system');
