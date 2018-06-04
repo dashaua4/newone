9249187a6dbc49806757af4ade8c83fe2d6a28da
dashaua4 <dashaua4@users.noreply.github.com>
2018-06-04 11:51:46 +0300

    Update function.php

--- function.php
+++ function.php
@@ -15,7 +15,18 @@ if (!$conn) {
 $sql ="SELECT id,name,price FROM $table";	
 $result = mysqli_query($conn, $sql);
 $date=mysqli_fetch_array($result);
-	echo $date["name"].'-'.$date["price"].'<br>';
+	
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . $row["price"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+	
          
 
 
