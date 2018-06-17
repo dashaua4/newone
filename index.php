61da1a7cf7488edd75c391a154abdfa05d9f79af
dashaua4 <dashaua4@users.noreply.github.com>
2018-06-17 14:09:14 +0300

    Update index.php

--- index.php
+++ index.php
@@ -6,42 +6,42 @@
 include('curl_query.php');
 include('simple_html_dom.php');
 
-include('main.php');
+//include('main.php');
 include('function.php');
 //Drop_table();
 
-//Cr_table('Workplace');
+Cr_table('Monitor');
 //echo 'GGGGGGGG';
 $html=curl_get('https://www.real-estate.lviv.ua/orenda-commercialproperty-office/Lviv-Lichakivskiy');
 
 $dom=str_get_html($html);
 
-$tables=$dom->find('.col-sm-12');
+$tables=$dom->find('.product_title');
 $i=51;
 foreach($tables as $table)
+{
+
 {
 $tobd=array();
 	$tobd['id']=$i++;
 	$a=$table->find('a',0);
 	
 	
-$tobd['adress']="'".$a->plaintext."'";
-	$one=curl_get('https://www.real-estate.lviv.ua'.$a->href);
+$tobd['name']="'".$a->plaintext."'";
+	$one=curl_get('https://deshevshe.net.ua'.$a->href);
 
-$n=$tobd['adress'];
+$n=$tobd['name'];
 		
 
 	$one_dom=str_get_html($one);
-	//echo $one;
-	$b='Личаківський';
-	//$area=$one_dom->find('.text-muted',0);
-	$tobd['area']="'".$b."'";
-	$cost=$one_dom->find('.h1-under-main-menu',0);
+	
+	$cost=$one_dom->find('.product__price_current',0);
+
 	$tobd['price']=(int)$cost->plaintext;
-	$size=$one_dom->find('.row row-dense',0);
-	$tobd['size']=(int)$size->plaintext;
-	$tobd['id_site']=3;
-echo $tobd[adress].'-'.$tobd[area].'-'.$tobd[price].'='.$tobd[size].'<br>';
+	$diagonal=$one_dom->find('.characteristic__product_value',0);
+	$tobd['diagonal']=(int)$size->plaintext;
+	$tobd['id_site']=1;
+echo $tobd[name].'-'.$tobd[price].'-'.$tobd[diagonal].'='.$tobd[id_site].'<br>';
 	//Insert('Workplace',$tobd);
 //echo $tobd['id'][5];
 }
