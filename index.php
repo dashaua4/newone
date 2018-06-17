aa81ec86e5182a9bd1a05af7c4de055a847bc08d
dashaua4 <dashaua4@users.noreply.github.com>
2018-06-17 14:14:28 +0300

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 include('curl_query.php');
 include('simple_html_dom.php');
 
-//include('main.php');
+include('main.php');
 include('function.php');
 //Drop_table();
 
@@ -25,21 +25,16 @@ foreach($tables as $table)
 $tobd=array();
 	$tobd['id']=$i++;
 	$a=$table->find('a',0);
-	
-	
+
 $tobd['name']="'".$a->plaintext."'";
 	$one=curl_get('https://deshevshe.net.ua'.$a->href);
-
 $n=$tobd['name'];
 		
-
 	$one_dom=str_get_html($one);
-	
 	$cost=$one_dom->find('.product__price_current',0);
-
 	$tobd['price']=(int)$cost->plaintext;
 	$diagonal=$one_dom->find('.characteristic__product_value',0);
-	$tobd['diagonal']=(int)$size->plaintext;
+	$tobd['diagonal']=(int)$diagonal->plaintext;
 	$tobd['id_site']=1;
 echo $tobd[name].'-'.$tobd[price].'-'.$tobd[diagonal].'='.$tobd[id_site].'<br>';
 	//Insert('Workplace',$tobd);
