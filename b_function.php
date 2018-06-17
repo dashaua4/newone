3d7f73717891c1b6bfacd3db64178fcdf1560c84
dashaua4 <dashaua4@users.noreply.github.com>
2018-06-17 15:11:29 +0300

    Update b_function.php

--- b_function.php
+++ b_function.php
@@ -12,29 +12,19 @@ include('function.php');
 </head>
    
 <body>
-   
-
    <div class="header">
  <h1>Бізнес Ідея</h1>
     <div class="row">
-	
-		
             <label  >
                 Шаблони (3)
             </label>
-<form method="post" action="b_function.php">
+   <form method="post" action="b_function.php">
             <select name="exampl"  onchange="this.form.submit()" >
-                <option value="-1">—</option>
-                                   <option value="1">
-                        Дизайн та Реклама 
-                    </option>
-                                    <option value="2">
-                       IT Компанія
-                    </option>
-                                    <option value="3">
-                       Офіс
-                    </option>
-                    </select>
+                		<option value="-1">—</option>
+                                   <option value="1">Дизайн та Реклама  </option>
+                                    <option value="2">IT Компанія</option>
+                                    <option value="3">Офіс</option>
+              </select>
      </form>
     </div>
 	
@@ -55,35 +45,51 @@ else{$wg='Office_comp';	}
 <div class="main">
 <table>
 <tr>
-                <th>Що купуємо</th>
-                <th>Мінімальна вартість</th>
-               <th>Максимальна вартість</th>
-            </tr>        
+	<th>Що купуємо</th>
+        <th>Мінімальна вартість</th>
+        <th>Максимальна вартість</th>
+</tr>        
 		<tr>
-                <td>
-                   
+                <td>                  
                     <h3>Обладнання</h3>
-					 <tr>
-                <td>
-                    Комп'ютери <span class="currency"></span>
-                </td>
-                <td>
-                    <input class="min" name="data[comp]" value="<? $SLT($wg);?>" type="text">
-                </td>
-                 <td>
-                    <input class="max" name="data[comp]" value="<? $SLTM($wg);?>" type="text">
-                </td>
-            </tr>
-					
-					 <tr>
-                <td>
-                    Монітори <span class="currency"></span>
-                </td>
-                <td>
-                    <input class="min" name="data[comp]" value="" type="text">
-                </td>
-                 <td>
-                    <input class="max" name="data[comp]" type="text">
+	    <tr>
+                <td>Комп'ютери <span class="currency"></span></td>
+                <td><input class="min" name="data[comp]" value="<? $SLT($wg);?>" type="text"></td>
+                 <td><input class="max" name="data[comp]" value="<? $SLTM($wg);?>" type="text"></td>
+            </tr>			
+	     <tr>
+                <td>Монітори <span class="currency"></span></td>
+                <td><input class="min" name="data[comp]" value="<? $SMT($mon,$diagonal);?>" type="text"></td>
+                 <td><input class="max" name="data[comp]" value="<? $SMTM($mon,$diagonal);?>" type="text"></td>
+		 <td>
+                    <form method="post" action="b_function.php">
+            <select name="monitor"  onchange="this.form.submit()" >
+                <option value="-1">—</option>
+                                <option value="15">15</option>
+                          	<option value="17">17</option>
+		    		<option value="18">18</option>
+		    		<option value="19">19</option>
+		    		<option value="20">20</option>
+		    		<option value="21">21</option>
+		    		<option value="23">23</option>
+		    		
+                    </select>
+    		 </form>
+			 <?php 
+			 $mon='Monitor'
+			 $value=3;
+$SMT='SNT';
+$SMTM='SMTM';
+	if(isset($_POST['monitor']))
+	{
+	$value = $_POST['monitor'];
+	switch($value)
+		case 15:
+		$diagonal=15;
+		break;
+	
+	}
+			 ?>
                 </td>
             </tr>
 					 <tr>
