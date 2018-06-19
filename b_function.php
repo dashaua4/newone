ae3b3a75a981cc5417b6b19234fb2749900ad8f3
dashaua4 <dashaua4@users.noreply.github.com>
2018-06-19 20:56:39 +0300

    Update b_function.php

--- b_function.php
+++ b_function.php
@@ -24,10 +24,12 @@ include('function.php');
 
    <form method="post" action="#">
            
-	    <select name="foo" onchange="this.form.submit()" > <option value="1" <?php if(isset($_POST['foo']) && $_POST['foo'] == 'Дизайн'){  $_SESSION['foo'] = '2';echo ' selected="selected"'; } ?> >Дизайн та Реклама</option>
-	   <option value="2" <?php if(isset($_POST['foo']) && $_POST == '2'){  $_SESSION['foo'] = 'ІТ';echo ' selected="selected"'; }?> ><?php echo $_SESSION['foo']; ?></option>
-		     <option value="3" <?php if(isset($_POST['foo'] )&& $_POST['foo'] == '3'){ $_SESSION['foo'] = 'Офіс'; echo ' selected="selected"';} ?> >Офіс</option>
-	   </select>
+	   <select name="exampl"  onchange="this.form.submit()" >
+                  <option value="-1">—</option>
+    <option value="1">Дизайн та Реклама  </option>
+                                <option value="2">IT Компанія</option>
+                                <option value="3">Офіс</option>            
+              </select>
   
      </form> 
     </div>
@@ -38,8 +40,8 @@ include('function.php');
 
 $value=3;
 	session_start(); 
-	if(isset($_POST['foo'])){
-$value = $_POST['foo'];
+	if(isset($_POST['exampl'])){
+$value = $_POST['exampl'];
 	if($value==1){$wg='WG_system';if (!isset($_SESSION['counter'])) $_SESSION['counter']=0;
 $_SESSION['counter']++;}
 	else if($value==2){ $wg='WG_system';if (!isset($_SESSION['counter'])) $_SESSION['counter']=0;
@@ -165,12 +167,12 @@ $SLTMC=SelectTMAX('Chairs');
 		 <td><input  type="radio" id='r2' onclick='foo(this.id);'  value='2' >Оренда</td>
 	</tr>
 	<script> 
-function foo(id) { 
-document.getElementById("txt").disabled = id=='r2' ? false : true; 
-	document.getElementById("txt1").disabled = id=='r2' ? false : true;
-	document.getElementById("txt2").disabled = id=='r2' ? false : true;
-	document.getElementById("txt3").disabled = id=='r2' ? false : true;
-	} 
+//function foo(id) { 
+//document.getElementById("txt").disabled = id=='r2' ? false : true; 
+	//document.getElementById("txt1").disabled = id=='r2' ? false : true;
+	//document.getElementById("txt2").disabled = id=='r2' ? false : true;
+	//document.getElementById("txt3").disabled = id=='r2' ? false : true;
+	//} 
  
 </script>
 	<tr>
@@ -180,29 +182,63 @@ document.getElementById("txt").disabled = id=='r2' ? false : true;
  <tr>
              <td> <form method="post"> 
             <select name="area" disabled id="txt3" onchange="this.form.submit()"  >
-                <option value="-1">—</option>
+                		<option value="-1">—</option>
                                 <option value="1">Галицький</option>
-                           <option value="2">Залізничний</option>
-        <option value="3">Личаківський</option>
-                    </select>
-   </form></td>
+                          	<option value="2">Залізничний</option>
+       				<option value="3">Личаківський</option>
+             </select>
+   		</form></td>
 	
   <td>  
 	  <form method="post">
             <select name="size"  disabled id="txt2" onchange="this.form.submit()" >
-                <option value="-1">—</option>
-                                <option value="0">0-50</option>
-                           <option value="50">50-100</option>
-        <option value="100">100-200</option>
-        <option value="200">200-300</option>
-        <option value="300">>300</option>
+              	  		<option value="-1">—</option>
+                                <option value="50">0-50</option>
+                           	<option value="100">50-100</option>
+        		 	<option value="200">100-200</option>
+       	 			<option value="300">200-300</option>
+        			<option value="400">>300</option>
                     </select>
    		</form>
-	 </td>
-            </tr> 
+   </td>
+</tr>  
+<?php 
+			$table='workplace';
+	$value=3;
+	if(isset($_POST['area'])){
+	$value = $_POST['area'];
+	if($value==1){$area='Галицький';if (!isset($_SESSION['counter'])) $_SESSION['counter']=0;
+	$_SESSION['counter']++;}
+	else if($value==2){ $area='Залізничний';if (!isset($_SESSION['counter'])) $_SESSION['counter']=0;
+	$_SESSION['counter']++;}
+	else{$area='Личаківський';if (!isset($_SESSION['counter'])) $_SESSION['counter']=0;
+	$_SESSION['counter']++;}
+	}
+	if(isset($_POST['size']))
+	{
+	$val = $_POST['size'];
+	$size=$val;
+	if (!isset($_SESSION['counter'])) $_SESSION['counter']=0;
+	$_SESSION['counter']++;
+	}
+			
+	$W_PSMin=W_PSMin($table,$area,$size);
+$W_PSMin=W_PSMax($table,$area,$size);
+
+	if($_SESSION['counter']>=1)
+	{unset($_SESSION['wp']); $_SESSION['counter']=0;
+	$_SESSION['wp'][]=array('wpmin'=>$W_PSMin, 'wpmax'=> $W_PSMin);
+	}		
+	
+	session_write_close($_SESSION['mas']);	
+?>
  	<tr>
-	      <td><input type='text' name='txt' id='txt1' disabled value='disabled'></td>
-             <td><input type='text' name='txt' id='txt' disabled value='disabled'></td>
+	      <td><input type='text' name='txt' id='txt1' disabled value=''>value="<? if(isset($_SESSION['wp']))
+		{foreach ($_SESSION['wp'] as $mas){
+	echo  $mas['wpmin']; }} ?>"</td>
+             <td><input type='text' name='txt' id='txt' disabled value=''>value="<? if(isset($_SESSION['wp']))
+		{foreach ($_SESSION['wp'] as $mas){
+	echo  $mas['wpmax']; }} ?>"</td>
             </tr>
 	
 	 </td>   
