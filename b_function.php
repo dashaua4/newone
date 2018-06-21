53a74b313000f5ac745d964456993e38b2574c2f
dashaua4 <dashaua4@users.noreply.github.com>
2018-06-21 13:50:16 +0300

    Update b_function.php

--- b_function.php
+++ b_function.php
@@ -67,7 +67,7 @@ $SLTM=SelectTMAX($wg);
                 <td>                  
                     <h3>Обладнання</h3>
 		 <form action="#" metod="post">
-  		  <p><input type="number" id="chet" min="0" max="10" step="2" value="6" onblur="alert(this.value)"></p>
+  		  <p><input type="number" id="chet" min="0" max="10" step="2" value="6" onblur="someFunc()"></p>
   		</form>
 	
 <script>
@@ -75,9 +75,9 @@ function someFunc(){
 var quantity = document.getElementById("chet").value;
 var price = quantity;
 alert(price);
-}
-	document.getElementById("by").appendChild(div);
 
+	document.getElementById("by").appendChild(div);
+}
 </script>
 
 
