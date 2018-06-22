e1dc7b8c881facc527791d806b93dc0db12baa1e
dashaua4 <dashaua4@users.noreply.github.com>
2018-06-22 14:50:26 +0300

    Update function.php

--- function.php
+++ function.php
@@ -68,7 +68,7 @@ if (!$conn) {
 $sql ="SELECT MAX(price) as price FROM $table WHERE diagonal=$diagonal";
 $result = mysqli_query($conn, $sql);
 $date=mysqli_fetch_array($result);
-	return $date["price"];    
+	return $date["price"]*$kol;    
 mysqli_close($conn);
 }
 function W_PSMin($table,$area,$size)
