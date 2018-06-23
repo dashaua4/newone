acee9e7cc61f4cbe846a265ca566b39de2fb46a4
dashaua4 <dashaua4@users.noreply.github.com>
2018-06-23 13:33:40 +0300

    Update left.php

--- left.php
+++ left.php
@@ -3,7 +3,7 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <title>Farfalla</title>
- <link href="css/style.css" rel="stylesheet" />
+ <link href="style.css" rel="stylesheet" />
 
 </head>
 <style>
@@ -11,7 +11,7 @@
 </style>
 <body >
 	
-	left
+
 
 </body>
 </html> 
