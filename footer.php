26e429c6132356455f16a18f363417b39c84fb48
dashaua4 <dashaua4@users.noreply.github.com>
2018-06-23 13:39:28 +0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,13 +1,13 @@
 <html>
  <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-  <title>ПП Олександр</title>
-   <link href="css/style.css" rel="stylesheet" />
+  <title>Business Plan</title>
+   <link href="style.css" rel="stylesheet" />
  </head>
 
 <body>
 <footer>
-Львів 2017р.
+Львів 2018р.
 </footer>
 </body>
 </html>
