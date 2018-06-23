9b7e9e5b521e012a4ab5313d306c78e324c7855d
dashaua4 <dashaua4@users.noreply.github.com>
2018-06-23 13:36:28 +0300

    Update left.php

--- left.php
+++ left.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>Farfalla</title>
+<title>Business Plan</title>
  <link href="style.css" rel="stylesheet" />
 
 </head>
