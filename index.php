fd79fbb1d197b7e8acf0cf70f1557dcf431c6586
dashaua4 <dashaua4@users.noreply.github.com>
2018-06-25 23:27:35 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,25 +1,5 @@
  <?php
-// запускаем word
-$word = new COM("word.application") or die("Unable to instantiate Word");
-echo "Loaded Word, version {$word->Version}\n";
-echo "1";
-//делаем его активным окном
-$word->Visible = 1;
-echo "2";
-//открываем пустой документ
-$word->Documents->Add();
-echo "3";
-//Что то с ним делаем
-$word->Selection->TypeText("This is a test...");
-$word->Documents[1]->SaveAs("Useless test.doc");
-echo "4";
-//закрываем word
-$word->Quit();
-echo "5";
-//высвобождаем ресурсы объекта
-$word = null;
-
-/*include('curl_query.php');
+include('curl_query.php');
 include('simple_html_dom.php');
 include('main.php');
 include('function.php');
@@ -50,5 +30,24 @@ $n=$tobd['name'];
 //echo $tobd[name].'-'.$tobd[price].'-'.$tobd[diagonal].'='.$tobd[id_site].'<br>';
 	//Insert('Monitor',$tobd);
 }
+/*// запускаем word
+$word = new COM("word.application") or die("Unable to instantiate Word");
+echo "Loaded Word, version {$word->Version}\n";
+echo "1";
+//делаем его активным окном
+$word->Visible = 1;
+echo "2";
+//открываем пустой документ
+$word->Documents->Add();
+echo "3";
+//Что то с ним делаем
+$word->Selection->TypeText("This is a test...");
+$word->Documents[1]->SaveAs("Useless test.doc");
+echo "4";
+//закрываем word
+$word->Quit();
+echo "5";
+//высвобождаем ресурсы объекта
+$word = null;
 */
 ?>
