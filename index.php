3928cac7013434bc6d802c20622da3b021882865
dashaua4 <dashaua4@users.noreply.github.com>
2018-06-25 23:01:06 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,44 @@
  <?php
 include('curl_query.php');
 include('simple_html_dom.php');
-include('main.php');
+<?php
+	require 'vendor/autoload.php';
+
+	use PHPStamp\Templator;
+	use PHPStamp\Document\WordDocument;
+
+	$cachePath = 'path/to/writable/directory/';
+	$templator = new Templator($cachePath); // опционально можно задать свой формат скобочек
+	// Для того чтобы каждый раз шаблон генерировался заново: 
+	// $templator->debug = true;
+
+	$documentPath = 'path/to/document.docx';
+	$document = new WordDocument($documentPath);
+
+	$values = array(
+		'library' => 'PHPStamp 0.1',
+		'simpleValue' => 'I am simple value',
+		'nested' => array(
+			'firstValue' => 'First child value',
+			'secondValue' => 'Second child value'
+		),
+		'header' => 'test of a table row',
+		'students' => array(
+			array('id' => 1, 'name' => 'Student 1', 'mark' => '10'),
+			array('id' => 2, 'name' => 'Student 2', 'mark' => '4'),
+			array('id' => 3, 'name' => 'Student 3', 'mark' => '7')
+		),
+		'maxMark' => 10,
+		'todo' => array(
+			'TODO 1',
+			'TODO 2',
+			'TODO 3'
+		)
+	);
+	$result = $templator->render($document, $values);
+	$result->download();
+
+/*include('main.php');
 include('function.php');
 //Drop_table();
 
@@ -30,5 +67,5 @@ $n=$tobd['name'];
 //echo $tobd[name].'-'.$tobd[price].'-'.$tobd[diagonal].'='.$tobd[id_site].'<br>';
 	//Insert('Monitor',$tobd);
 }
-
+*/
 ?>
